6ae90cf28c83d94b72a4b80e0d9899860fdf255b
Azury <azury.wtf@gmail.com>
2021-11-21 11:26:03 +0100

    added types

---
+++ types/schedule.d.ts
@@ -0,0 +1,10 @@
+declare const _default: (interval: string, task: Function) => Promise<boolean>;
+/**
+ * **Schedule Task**
+ *
+ * Schedule a task in a specific interval.
+ *
+ * @param interval
+ * @param task
+ */
+export default _default;
